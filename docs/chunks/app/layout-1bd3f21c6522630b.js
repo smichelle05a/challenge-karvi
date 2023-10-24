cc96a82843659e81dcc8dfc76f23f68a81c0517c
Scarlet Albornoz <salbornoz@lanacion.com.ar>
2023-10-24 01:50:50 -0300

    build first try (?)

---
+++ docs/chunks/app/layout-1bd3f21c6522630b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5358:function(n,e,t){Promise.resolve().then(t.t.bind(t,6916,23)),Promise.resolve().then(t.t.bind(t,5349,23))},5349:function(){},6916:function(){}},function(n){n.O(0,[971,472,744],function(){return n(n.s=5358)}),_N_E=n.O()}]);
